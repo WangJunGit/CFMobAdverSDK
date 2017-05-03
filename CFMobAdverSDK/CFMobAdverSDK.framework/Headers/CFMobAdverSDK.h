dd10b8bf1ca4cdca2670b05ee91897d827d329c8
WangJunGit <jun_page@163.com>
2017-05-03 16:06:34 +0800

    添加项目

---
+++ CFMobAdverSDK/CFMobAdverSDK.framework/Headers/CFMobAdverSDK.h
@@ -0,0 +1,38 @@
+//
+//  CFMobAdverSDK.h
+//  CFMobAdverSDK
+//
+//  Created by quemos on 2017/5/3.
+//  Copyright © 2017年 quemos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+#import "CFAdvertisement.h"
+
+#import "CFMobAdBannerDelegate.h"
+
+#import "CFMobAdBannerView.h"
+
+#import "CFMobAdCommonConfig.h"
+
+#import "CFMobAdInterstitiaDelegate.h"
+
+#import "CFMobAdInterstitialView.h"
+
+#import "CFMobAdSplashDelegate.h"
+
+#import "CFMobAdSplashView.h"
+
+
+
+//! Project version number for CFMobAdverSDK.
+FOUNDATION_EXPORT double CFMobAdverSDKVersionNumber;
+
+//! Project version string for CFMobAdverSDK.
+FOUNDATION_EXPORT const unsigned char CFMobAdverSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CFMobAdverSDK/PublicHeader.h>
+
+
