bca6ba0909072d46c6e61e783def67ab0e191467
WangJunGit <jun_page@163.com>
2017-05-04 12:53:49 +0800

    更改id

--- CFMobAdverSDK/CFMobAdverSDK.framework/Headers/CFMobAdverSDK.h
+++ CFMobAdverSDK/CFMobAdverSDK.framework/Headers/CFMobAdverSDK.h
@@ -2,30 +2,27 @@
 //  CFMobAdverSDK.h
 //  CFMobAdverSDK
 //
-//  Created by quemos on 2017/5/3.
+//  Created by quemos on 2017/5/4.
 //  Copyright © 2017年 quemos. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-
 #import "CFAdvertisement.h"
 
-#import "CFMobAdBannerDelegate.h"
+#import "CFMobAdCommonConfig.h"
 
 #import "CFMobAdBannerView.h"
 
 #import "CFMobAdCommonConfig.h"
 
-#import "CFMobAdInterstitiaDelegate.h"
+#import "CFMobAdCommonConfig.h"
 
 #import "CFMobAdInterstitialView.h"
 
-#import "CFMobAdSplashDelegate.h"
+#import "CFMobAdCommonConfig.h"
 
 #import "CFMobAdSplashView.h"
-
-
 
 //! Project version number for CFMobAdverSDK.
 FOUNDATION_EXPORT double CFMobAdverSDKVersionNumber;
